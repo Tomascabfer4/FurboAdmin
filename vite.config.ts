835e1bfaa360cc621d03ffde249911c5efe338c4
tomascabfer4 <tomascabfer4@gmail.com>
2026-01-30 19:40:50 +0100

    Arreglo de estilos

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // Asegura que las rutas sean absolutas desde la raíz
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+});
